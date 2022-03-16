b249bfa2445befdb15be0df8219b76fe9ab79aa1
Denis Tsyganok <denis.tsyganok@leasoft.org>
2022-03-16 11:07:53 +0200

    git added translations for plugin extension

---
+++ upload/admin/language/de-de/extension/payment/ginger.php
@@ -0,0 +1,57 @@
+<?php
+
+/**
+ * General Admin Settings Page
+ */
+$_['heading_title'] = 'EMS Online: Plugin';
+$_['text_ginger'] = '<img src="view/image/payment/ginger_Plugin.png" alt="EMS Online" title="EMS Online" />';
+$_['text_extension'] = 'Extensions';
+
+/**
+ * Entry points
+ */
+$_['entry_status'] = 'Status:';
+$_['entry_order_completed'] = 'Bestellung abgeschlossen:';
+$_['entry_order_new'] = 'Neu Bestellung:';
+$_['entry_order_expired'] = 'Bestellung abgelaufen:';
+$_['entry_order_cancelled'] = 'Bestellung annulliert:';
+$_['entry_order_processing'] = 'Bestellung wird bearbeitet:';
+$_['entry_order_error'] = 'Bestellfehler:';
+$_['entry_sort_order'] = 'Sortierreihenfolge:';
+$_['entry_ginger_api_key'] = 'EMS Online API Schüssel:';
+$_['entry_ginger_total'] = 'Gesamt:';
+$_['entry_cacert'] = 'Bundle cURL ca.cert:';
+$_['entry_order_captured'] = 'Bestellung gefangen:';
+
+/**
+ * Text strings
+ */
+$_['text_button_save'] = 'Speichern';
+$_['text_button_cancel'] = 'Abbrechen';
+$_['text_enabled'] = 'Aktiviert';
+$_['text_disabled'] = 'Deaktiviert';
+$_['text_payments'] = 'Zahlungen';
+$_['text_issuer_id'] = 'SWIFT/BIC';
+$_['text_settings_saved'] = 'EMS Online: Plugin Einstellungen aktualisiert!';
+$_['text_edit_ginger'] = 'Bearbeiten EMS Online: Plugin Einstellungen';
+$_['text_yes'] = 'Ja';
+$_['text_no'] = 'Nee';
+
+/**
+ * Error messages
+ */
+$_['error_missing_api_key'] = 'EMS Online API schlüssel ist erforderlich!';
+
+/**
+ * Information text
+ */
+$_['info_help_api_key'] = 'Duplizieren Sie Ihre EMS Online API Schlüssel von Merchant Portal.';
+$_['info_help_total'] = 'Die Zahlungsmethode wird gezeigt wenn die kasse insgesamt der Mindestbetrag erreicht.';
+$_['info_plugin_not_configured'] = 'EMS Online: Plugin plugin ist nicht konfiguriert.';
+
+/**
+ * Refund text
+ */
+$_['empty_price'] = 'Der Preis ist in den Produktinformationen zur Rückerstattung leer.';
+$_['wrong_order_status'] = 'Nur abgeschlossene Bestellungen können erstattet werden.';
+$_['refund_not_completed'] = 'Rückerstattungsauftrag ist nicht abgeschlossen.';
